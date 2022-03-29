cb47432755c92b7ce07d8eb3d35ee8b30a93bf08
Kaan Yalçınkaya <74143983+kaanssss@users.noreply.github.com>
2022-03-29 16:53:13 +0300

    Add files via upload

---
+++ CollectionViewCalisma/CollectionViewCalisma/MyCell.swift
@@ -0,0 +1,49 @@
+//
+//  MyCell.swift
+//  CollectionViewCalisma
+//
+//  Created by Kaan Yalçınkaya on 18.02.2022.
+//
+
+import UIKit
+
+class MyCell: UICollectionViewCell {
+
+    weak var textLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        let textLabel = UILabel(frame: .zero)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+        ])
+        self.textLabel = textLabel
+
+        self.contentView.backgroundColor = .lightGray
+        self.textLabel.textAlignment = .center
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        fatalError("Interface Builder is not supported!")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        fatalError("Interface Builder is not supported!")
+    }
+
+        override func prepareForReuse() {
+            super.prepareForReuse()
+
+            self.textLabel.text = nil
+        }
+    }
---
+++ CollectionViewCalisma/CollectionViewCalisma/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  CollectionViewCalisma
+//
+//  Created by Kaan Yalçınkaya on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    weak var collectionView: UICollectionView!
+
+        override func loadView() {
+            super.loadView()
+
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+            collectionView.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(collectionView)
+            NSLayoutConstraint.activate([
+                collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            ])
+            self.collectionView = collectionView
+        }
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            self.collectionView.backgroundColor = .white
+            self.collectionView.dataSource = self
+            self.collectionView.delegate = self
+
+            self.collectionView.register(MyCell.self, forCellWithReuseIdentifier: "MyCell")
+        }
+    }
+    extension ViewController: UICollectionViewDataSource {
+        
+        func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 1
+        }
+
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 10
+        }
+
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! MyCell
+            cell.textLabel.text = String(indexPath.row + 1)
+            return cell
+        }
+    }
+
+    extension ViewController: UICollectionViewDelegate {
+
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            print(indexPath.row + 1)
+        }
+    }
+
+    extension ViewController: UICollectionViewDelegateFlowLayout {
+
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+            return CGSize(width: collectionView.bounds.size.width - 16, height: 120)
+        }
+        func collectionView(_ collectionView: UICollectionView,
+                            layout collectionViewLayout: UICollectionViewLayout,
+                            minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 8
+        }
+
+            func collectionView(_ collectionView: UICollectionView,
+                                layout collectionViewLayout: UICollectionViewLayout,
+                                minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+                return 0
+            }
+
+            func collectionView(_ collectionView: UICollectionView,
+                                layout collectionViewLayout: UICollectionViewLayout,
+                                insetForSectionAt section: Int) -> UIEdgeInsets {
+                return UIEdgeInsets.init(top: 8, left: 8, bottom: 8, right: 8)
+            }
+        }
